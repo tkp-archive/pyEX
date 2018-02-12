cbb50ef7964da69346bba8321b1a206fd3c7a5ce
Tim Paine <t.paine154@gmail.com>
2018-02-12 17:51:25 -0500

    working on ws api

--- pyEX/common.py
+++ pyEX/common.py
@@ -1,6 +1,6 @@
 import requests
 import pandas as pd
-
+import websocket
 
 _URL_PREFIX = 'https://api.iextrading.com/1.0/'
 _TIMEFRAME_CHART = ['5y', '2y', '1y', 'ytd', '6m', '3m', '1m', '1d']
@@ -30,3 +30,35 @@ def _strToList(st):
 def _raiseIfNotStr(s):
     if s is not None and not isinstance(s, str):
         raise Exception('Cannot use type %s' % str(type(s)))
+
+
+class WSClient(object):
+    def __init__(self, addr, sendinit=None, on_data=None, on_open=None, on_close=None, on_error=None):
+        self.on_data = on_data or print
+
+        def on_message(ws, message):
+            self.on_data(message)
+
+        def on_error_default(ws, error):
+            pass
+
+        def on_close_default(ws):
+            pass
+
+        def on_open_default(ws):
+            if sendinit:
+                ws.send(sendinit)
+
+        self.ws = websocket.WebSocketApp(addr,
+                                         on_message=on_message,
+                                         on_error=on_error if on_error else on_error_default,
+                                         on_close=on_close if on_close else on_close_default)
+        self.ws.on_open = on_open if on_open else on_open_default
+
+    def run(self):
+        self.ws.run_forever()
+
+
+def _stream(url, sendinit=None, on_data=print):
+    factory = WSClient(url, sendinit=sendinit, on_data=on_data)
+    return factory
---
+++ pyEX/marketdata/ws.py
@@ -0,0 +1,107 @@
+from datetime import datetime
+from ..common import _getJson, _strToList, _raiseIfNotStr
+
+
+def tops(symbols=None, on_data=None):
+    '''https://iextrading.com/developer/docs/#tops'''
+    symbols = _strToList(symbols)
+    if symbols:
+        return _getJson('tops?symbols=' + ','.join(symbols) + '%2b')
+    return _getJson('tops')
+
+
+def last(symbols=None, on_data=None):
+    '''https://iextrading.com/developer/docs/#last'''
+    symbols = _strToList(symbols)
+    if symbols:
+        return _getJson('tops/last?symbols=' + ','.join(symbols) + '%2b')
+    return _getJson('tops/last')
+
+
+def hist(date=None, on_data=None):
+    '''https://iextrading.com/developer/docs/#hist'''
+    if date is None:
+        return _getJson('hist')
+    elif isinstance(date, datetime):
+        return _getJson('hist?date=' + date.strftime('%Y%m%d'))
+    elif isinstance(date, str):
+        return _getJson('hist?date=' + date)
+    else:
+        raise Exception("Can't handle type : %s" % str(type(date)))
+
+
+def deep(symbol=None, on_data=None):
+    '''https://iextrading.com/developer/docs/#deep'''
+    _raiseIfNotStr(symbol)
+    if symbol:
+        return _getJson('deep?symbols=' + symbol)
+    return _getJson('deep')
+
+
+def book(symbol=None, on_data=None):
+    '''https://iextrading.com/developer/docs/#book51'''
+    _raiseIfNotStr(symbol)
+    if symbol:
+        return _getJson('deep/book?symbols=' + symbol)
+    return _getJson('deep/book')
+
+
+def trades(symbol=None, on_data=None):
+    '''https://iextrading.com/developer/docs/#trades'''
+    _raiseIfNotStr(symbol)
+    if symbol:
+        return _getJson('deep/trades?symbols=' + symbol)
+    return _getJson('deep/trades')
+
+
+def systemEvent(on_data=None):
+    '''https://iextrading.com/developer/docs/#system-event'''
+    return _getJson('deep/system-event')
+
+
+def tradingStatus(symbol=None, on_data=None):
+    '''https://iextrading.com/developer/docs/#trading-status'''
+    _raiseIfNotStr(symbol)
+    if symbol:
+        return _getJson('deep/trading-status?symbols=' + symbol)
+    return _getJson('deep/trading-status')
+
+
+def opHaltStatus(symbol=None, on_data=None):
+    '''https://iextrading.com/developer/docs/#operational-halt-status'''
+    _raiseIfNotStr(symbol)
+    if symbol:
+        return _getJson('deep/op-halt-status?symbols=' + symbol)
+    return _getJson('deep/op-halt-status')
+
+
+def ssrStatus(symbol=None, on_data=None):
+    '''https://iextrading.com/developer/docs/#short-sale-price-test-status'''
+    _raiseIfNotStr(symbol)
+    if symbol:
+        return _getJson('deep/ssr-status?symbols=' + symbol)
+    return _getJson('deep/ssr-status')
+
+
+def securityEvent(symbol=None, on_data=None):
+    '''https://iextrading.com/developer/docs/#security-event'''
+    _raiseIfNotStr(symbol)
+    if symbol:
+        return _getJson('deep/security-event?symbols=' + symbol)
+    return _getJson('deep/security-event')
+
+
+def tradeBreak(symbol=None, on_data=None):
+    '''https://iextrading.com/developer/docs/#trade-break'''
+    _raiseIfNotStr(symbol)
+    if symbol:
+        return _getJson('deep/trade-breaks?symbols=' + symbol)
+    return _getJson('deep/trade-breaks')
+
+
+def auction(symbol=None, on_data=None):
+    '''https://iextrading.com/developer/docs/#auction'''
+    _raiseIfNotStr(symbol)
+    if symbol:
+        return _getJson('deep/auction?symbols=' + symbol)
+    return _getJson('deep/auction')
