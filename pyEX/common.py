5bb4bf06aa7f7c042f2df4e57a2b577649a80212
Tim Paine <t.paine154@gmail.com>
2018-02-15 19:56:21 -0500

    working on websocket connection

--- pyEX/common.py
+++ pyEX/common.py
@@ -1,9 +1,11 @@
 import requests
 import pandas as pd
 import websocket
+import http.client as httplib
+
 
 _URL_PREFIX = 'https://api.iextrading.com/1.0/'
-_WS_URL_PREFIX = 'wss://ws-api.iextrading.com/1.0/'
+_SIO_URL_PREFIX = 'https://ws-api.iextrading.com/1.0/'
 _TIMEFRAME_CHART = ['5y', '2y', '1y', 'ytd', '6m', '3m', '1m', '1d']
 _TIMEFRAME_DIVSPLIT = ['5y', '2y', '1y', 'ytd', '6m', '3m', '1m']
 _LIST_OPTIONS = ['mostactive', 'gainers', 'losers', 'iexvolume', 'iexpercent']
@@ -18,7 +20,7 @@ def _getJson(url):
 
 
 def _wsURL(url):
-        return _WS_URL_PREFIX + url
+        return _SIO_URL_PREFIX + url
 
 
 def _df(resp):
@@ -41,20 +43,31 @@ class WSClient(object):
     def __init__(self, addr, sendinit=None, on_data=None, on_open=None, on_close=None, on_error=None):
         self.on_data = on_data or print
 
+        print(addr)
+        conn = httplib.HTTPConnection(addr)
+        conn.request('POST', '/socket.io/1/')
+        resp = conn.getresponse()
+        hskey = resp.read().split(':')[0]
+
         def on_message(ws, message):
+            print(message)
             self.on_data(message)
 
         def null(ws, *args):
-            pass
+            print(args)
 
         def on_open_default(ws):
             if sendinit:
                 ws.send(sendinit)
 
-        self.ws = websocket.WebSocketApp(addr, on_message=on_message, on_error=on_error if on_error else null, on_close=on_close if on_close else null)
+        self.ws = websocket.WebSocketApp(_WS_URL_PREFIX+hskey,
+                                         on_message=on_message,
+                                         on_error=on_error if on_error else null,
+                                         on_close=on_close if on_close else null)
         self.ws.on_open = on_open if on_open else on_open_default
 
     def run(self):
+
         self.ws.run_forever()
 
 
