59956af4527c59fdeb9881102549806b0ee91ac7
Tim Paine <t.paine154@gmail.com>
2018-02-19 18:21:04 -0500

    upping version

--- pyEX/__init__.py
+++ pyEX/__init__.py
@@ -4,3 +4,5 @@ from .stats import *
 from .markets import *
 from .marketdata.http import tops, last, hist, deep, book as topsBook, trades, systemEvent, tradingStatus, opHaltStatus, ssrStatus, securityEvent, tradeBreak, auction
 from .marketdata.ws import *
+
+__version__ = '0.0.4'
--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pyEX',
-    version='0.0.3',
+    version='0.0.4',
     description='Rest API to IEX',
     long_description=long_description,
     url='https://github.com/timkpaine/pyEX',
-    download_url='https://github.com/timkpaine/pyEX/archive/v0.0.3.tar.gz',
+    download_url='https://github.com/timkpaine/pyEX/archive/v0.0.4.tar.gz',
     author='Tim Paine',
     author_email='timothy.k.paine@gmail.com',
     license='LGPL',
