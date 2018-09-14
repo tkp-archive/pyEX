cfd61c198753a531c545a1e9fc9be0b2f83680a9
Tim Paine <t.paine154@gmail.com>
2018-09-14 15:42:26 -0400

    .

--- pyEX/__init__.py
+++ pyEX/__init__.py
@@ -4,5 +4,6 @@ from .stats import *
 from .markets import *
 from .marketdata.http import tops, topsDF, last, lastDF, hist, histDF, deep, deepDF, book as topsBook, bookDF as topsBookDF, trades, tradesDF, systemEvent, systemEventDF, tradingStatus, tradingStatusDF, opHaltStatus, opHaltStatusDF, ssrStatus, ssrStatusDF, securityEvent, securityEventDF, tradeBreak, tradeBreakDF, auction, auctionDF, officialPrice, officialPriceDF
 from .marketdata.ws import *
+from __future__ import print_function
 
 __version__ = '0.1.0'
