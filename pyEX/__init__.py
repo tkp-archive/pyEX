12b8baa4b28470ebe333b8d84240e6fed4c16ac9
Tim Paine <t.paine154@gmail.com>
2018-09-14 15:43:15 -0400

    .

--- pyEX/__init__.py
+++ pyEX/__init__.py
@@ -1,9 +1,9 @@
+from __future__ import print_function
 from .stocks import *
 from .refdata import symbols, symbolsDF, corporateActions, corporateActionsDF, dividends as dividendsRef, dividendsDF as dividendsRefDF, nextDayExtDate, nextDayExtDateDF, directory, directoryDF
 from .stats import *
 from .markets import *
 from .marketdata.http import tops, topsDF, last, lastDF, hist, histDF, deep, deepDF, book as topsBook, bookDF as topsBookDF, trades, tradesDF, systemEvent, systemEventDF, tradingStatus, tradingStatusDF, opHaltStatus, opHaltStatusDF, ssrStatus, ssrStatusDF, securityEvent, securityEventDF, tradeBreak, tradeBreakDF, auction, auctionDF, officialPrice, officialPriceDF
 from .marketdata.ws import *
-from __future__ import print_function
 
 __version__ = '0.1.0'
