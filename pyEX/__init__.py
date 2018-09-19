679ffb187d5c9aa9e274713ef3c136a456ad2bd2
Tim Paine <t.paine154@gmail.com>
2018-09-19 16:33:43 -0400

    bump version

--- pyEX/__init__.py
+++ pyEX/__init__.py
@@ -5,4 +5,4 @@ from .markets import *
 from .marketdata.http import tops, topsDF, last, lastDF, hist, histDF, deep, deepDF, book as topsBook, bookDF as topsBookDF, trades, tradesDF, systemEvent, systemEventDF, tradingStatus, tradingStatusDF, opHaltStatus, opHaltStatusDF, ssrStatus, ssrStatusDF, securityEvent, securityEventDF, tradeBreak, tradeBreakDF, auction, auctionDF, officialPrice, officialPriceDF
 from .marketdata.ws import *
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
