d16af788094397051ab06c5003816fb3c303dcae
Tim Paine <t.paine154@gmail.com>
2019-02-11 18:28:25 -0500

    coverage

--- tests/test_client.py
+++ tests/test_client.py
@@ -8,6 +8,17 @@ class TestClient:
         from pyEX import Client
         c = Client('test')
 
+    def test_client_notoken(self):
+        import os
+        from pyEX import Client, PyEXception
+        tmp = os.environ.pop('IEX_TOKEN', '')
+        try:
+            Client()
+            assert False
+        except PyEXception:
+            pass
+        os.environ['IEX_TOKEN'] = tmp
+
     def test_account(self):
         from pyEX import Client
         c = Client('test')
