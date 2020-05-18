87cf31cd050b997802f76641613ef3322056a57a
Tim Paine <t.paine154@gmail.com>
2020-05-18 16:15:57 -0400

    fix lint

--- pyEX/__init__.py
+++ pyEX/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from .common import PyEXception, PyEXStopSSE
+from .common import PyEXception, PyEXStopSSE  # noqa: F401
 from .client import *  # noqa: F401, F403
 from .alternative import (crypto, cryptoDF,  # noqa: F401
     sentiment, sentimentDF,  # noqa: F401
