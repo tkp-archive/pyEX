84865ab24d062598f208f95d3b3f4f6202873c28
Tim Paine <t.paine154@gmail.com>
2020-11-17 00:16:34 -0500

    starting on tick enhancrs

---
+++ pyEX/studies/tick.py
@@ -0,0 +1,7 @@
+
+def makeTick(open, high, low, close, volume, count=100):
+    '''generate a random path from `open` to `close`,
+    touching both `high` and `low`, over a total volume
+    of `volume` points.
+    '''
+    pass
