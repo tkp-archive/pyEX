f7487b389d57f771378936a73aa03c64dfbd2313
Tim Paine <t.paine154@gmail.com>
2021-02-15 16:16:11 -0500

    fix copyright notice

--- pyEX/__init__.py
+++ pyEX/__init__.py
@@ -2,7 +2,7 @@
 #
 # Copyright (c) 2020, the pyEX authors.
 #
-# This file is part of the jupyterlab_templates library, distributed under the terms of
+# This file is part of the pyEX library, distributed under the terms of
 # the Apache License 2.0.  The full license can be found in the LICENSE file.
 #
 from .client import *  # noqa: F403
