2139d01f646d6ecc60c221becd8b27771001ab75
Tim Paine <t.paine154@gmail.com>
2021-03-05 18:50:02 -0500

    missing import

---
+++ pyEX/files/files.py
@@ -0,0 +1,32 @@
+# *****************************************************************************
+#
+# Copyright (c) 2021, the pyEX authors.
+#
+# This file is part of the pyEX library, distributed under the terms of
+# the Apache License 2.0.  The full license can be found in the LICENSE file.
+#
+from ..common import _get, _strOrDate
+
+
+def files(id="", symbol="", date=None, token="", version="stable"):
+    """The Files API allows users to download bulk data files, PDFs, etc.
+
+    https://iexcloud.io/docs/api/#files
+
+    Args:
+        id (str): report ID
+        symbol (str): symbol to use
+        date (str): date of report to use
+    """
+    if id:
+        if symbol and date:
+            return _get(
+                "files/download/{}?symbol={}&date={}".format(
+                    id, symbol, _strOrDate(date)
+                ),
+                token=token,
+                version=version,
+            )
+        return _get("files/info/{}".format(id), token=token, version=version)
+
+    return _get("files", token=token, version=version)
