df421370b50d6b2359504cb92ce7e060843757fd
timkpaine <tim.paine@iexcloud.io>
2021-05-04 11:09:02 -0400

    fix brittle import, fix client method

--- pyEX/refdata/__init__.py
+++ pyEX/refdata/__init__.py
@@ -5,16 +5,49 @@
 # This file is part of the pyEX library, distributed under the terms of
 # the Apache License 2.0.  The full license can be found in the LICENSE file.
 #
-from .calendar import *
-from .exchanges import *
-from .figi import *
-from .isin import *
+from .calendar import calendar, calendarDF, holidays, holidaysDF
+from .exchanges import (
+    exchanges,
+    exchangesDF,
+    internationalExchanges,
+    internationalExchangesDF,
+)
+from .figi import figi, figiDF
+from .isin import isinLookup, isinLookupDF
 from .refdata import corporateActions, corporateActionsDF, directory, directoryDF
 from .refdata import dividends as refDividends
 from .refdata import dividendsDF as refDividendsDF
 from .refdata import nextDayExtDate, nextDayExtDateDF
-from .ric import *
+from .ric import ricLookup, ricLookupDF
 
-from .search import *
-from .sectors import *
-from .symbols import *
+from .search import search, searchDF
+from .sectors import sectors, sectorsDF, tags, tagsDF
+from .symbols import (
+    cryptoSymbols,
+    cryptoSymbolsDF,
+    cryptoSymbolsList,
+    futuresSymbols,
+    futuresSymbolsDF,
+    futuresSymbolsList,
+    fxSymbols,
+    fxSymbolsDF,
+    fxSymbolsList,
+    iexSymbols,
+    iexSymbolsDF,
+    iexSymbolsList,
+    internationalSymbols,
+    internationalSymbolsDF,
+    internationalSymbolsList,
+    mutualFundSymbols,
+    mutualFundSymbolsDF,
+    mutualFundSymbolsList,
+    optionsSymbols,
+    optionsSymbolsDF,
+    optionsSymbolsList,
+    otcSymbols,
+    otcSymbolsDF,
+    otcSymbolsList,
+    symbols,
+    symbolsDF,
+    symbolsList,
+)
