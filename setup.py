cd0a9486db32de6ccad12fd896cd42a39940fb27
Tim Paine <t.paine154@gmail.com>
2021-05-21 22:48:10 -0400

    bump to 3.9, drop 3.6

--- setup.py
+++ setup.py
@@ -83,9 +83,9 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords="finance data",
     zip_safe=False,
