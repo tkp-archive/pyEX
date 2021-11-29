c5c5aa5d6796554a3efe62c294344896f3152952
Tim Paine <t.paine154@gmail.com>
2021-11-28 21:51:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -80,6 +80,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     keywords="finance data",
     zip_safe=False,
@@ -89,4 +90,5 @@ setup(
         "dev": requires_dev,
         "studies": requires_studies,
     },
+    python_requires=">=3.7",
 )
