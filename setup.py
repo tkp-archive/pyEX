0229fc1eb576fd3087c58e2028edf92fdf20631a
Tim Paine <t.paine154@gmail.com>
2021-12-18 22:28:03 -0500

    .

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     description="Rest API to IEX",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/iexcloud/{name}".format(name=name),
+    url="https://github.com/timkpaine/{name}".format(name=name),
     author="Tim Paine",
     author_email="t.paine154@gmail.com",
     license="Apache 2.0",
